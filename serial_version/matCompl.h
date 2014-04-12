32dd615a6101324a006e18170e721d361271f71d
Matthew Brown <mbrown@math.umass.edu>
2014-04-12 17:44:07 -0400

    completed DGESVD function but not working
    can't find min, max, or DGESVD functions

--- serial_version/matCompl.h
+++ serial_version/matCompl.h
@@ -1,7 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
-
 #include "clapack.h"
 #include "matFunctions.h"
 
--- serial_version/test_mat.c
+++ serial_version/test_mat.c
@@ -8,18 +8,20 @@
 */
 double** test_mat(int r, int nrows, int ncols){
 	int i, j;
+	int info;
+	int LWORK = max(1,3*min(nrows,ncols)+max(nrows,ncols),5*min(nrows,ncols));
+	double** WORK = alloc_array(1, LWORK);
 	double** A  = alloc_array(nrows, ncols);		//create an mxm matrix
 	double** U = alloc_array(nrows, nrows);
 	double** VT = alloc_array(ncols, ncols);
-	double** S = alloc_array(
+	double** S = alloc_array(min(nrows,ncols), min(nrows, ncols));
 	for( i = 0; i < nrows; i++){
 		for( j = 0; j < ncols; j++ ){
 			A[i][j] = (double)rand()/RAND_MAX;
 		}
 	}
 	//      SUBROUTINE DGESVD( JOBU, JOBVT, M, N, A, LDA, S, U, LDU, VT, LDVT, WORK, LWORK, INFO )
-	DGESVD('A', 'A', nrows, ncols, A, nrows,  
-	
+	DGESVD('A', 'A', nrows, ncols, A, nrows, S, U, nrows, VT, ncols, WORK, LWORK, &info);
 	
 	return A;
 }

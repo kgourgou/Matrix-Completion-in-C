2b527e39f0ad88520eea42a1869c3c6eecb7d192
Kostantinos Gourgoulias <kgourgoulias@gmail.com>
2014-04-12 16:03:44 -0400

    Added matAlloc functions.

--- serial_version/main.c
+++ serial_version/main.c
@@ -7,8 +7,10 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+extern double **alloc_array(int rows, int col);
 
 int main(void){ 
+ double **A = alloc_array(3,3);
 
 
  return 0;
@@ -16,3 +18,27 @@ int main(void){
 }
 
 
+double** alloc_array(int nrows, int ncol){
+
+ int i,j;
+ double **A = (int**) (malloc(rows* sizeof(*double)));
+
+ for( i = 0; i <  rows; i++){
+  *(A[i]) = (malloc(ncol * sizeof(double)));
+ }
+
+ return A;
+}
+
+
+void free_array(double** A, int nrows){
+ int i;
+
+ for(i = 0; i < nrows; i++){
+  free(A[i]);
+ }
+
+ free(A);
+ return 0;
+
+}

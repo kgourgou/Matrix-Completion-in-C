563096501a2ac9c2f15d946ba5e69bf715e72eb6
Matthew Brown <mbrown@math.umass.edu>
2014-04-12 17:16:54 -0400

    started adding svd to test_mat.c

--- serial_version/test_mat.c
+++ serial_version/test_mat.c
@@ -6,13 +6,20 @@
   singular value decomposition is used. 
 
 */
-double** test_mat(int r, int numRows, int numCols){
+double** test_mat(int r, int nrows, int ncols){
 	int i, j;
-	double** A  = alloc_array(numRows, numCols);		//create an mxm matrix
-	for( i = 0; i < numRows; i++){
-		for( j = 0; j < numCols; j++ ){
+	double** A  = alloc_array(nrows, ncols);		//create an mxm matrix
+	double** U = alloc_array(nrows, nrows);
+	double** VT = alloc_array(ncols, ncols);
+	double** S = alloc_array(
+	for( i = 0; i < nrows; i++){
+		for( j = 0; j < ncols; j++ ){
 			A[i][j] = (double)rand()/RAND_MAX;
 		}
 	}
+	//      SUBROUTINE DGESVD( JOBU, JOBVT, M, N, A, LDA, S, U, LDU, VT, LDVT, WORK, LWORK, INFO )
+	DGESVD('A', 'A', nrows, ncols, A, nrows,  
+	
+	
 	return A;
 }

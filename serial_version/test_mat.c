293e42c2bf9bf19abe1eaa77962de533556c9e86
Matthew Brown <mbrown@math.umass.edu>
2014-04-17 00:03:31 -0400

    added gitignore to itself

--- serial_version/test_mat.c
+++ serial_version/test_mat.c
@@ -8,13 +8,14 @@
 */
 double** test_mat(int r, int nrows, int ncols){
 	int i, j;
-	int info;
-	int LWORK = max(1,3*min(nrows,ncols)+max(nrows,ncols),5*min(nrows,ncols));
+	int* info;
+	int LWORK = fmax(fmax(1,3*fmin(nrows,ncols)+fmax(nrows,ncols)),5*fmin(nrows,ncols));
+	//printf("%i",LWORK);
 	double** WORK = alloc_array(1, LWORK);
 	double** A  = alloc_array(nrows, ncols);		//create an mxm matrix
 	double** U = alloc_array(nrows, nrows);
 	double** VT = alloc_array(ncols, ncols);
-	double** S = alloc_array(min(nrows,ncols), min(nrows, ncols));
+	double** S = alloc_array(fmin(nrows,ncols), fmin(nrows, ncols));
 	
 	for( i = 0; i < nrows; i++){
 		for( j = 0; j < ncols; j++ ){
@@ -22,7 +23,12 @@ double** test_mat(int r, int nrows, int ncols){
 		}
 	}
 	
-	DGESVD('A', 'A', nrows, ncols, A, nrows, S, U, nrows, VT, ncols, WORK, LWORK, &info);
+	//int dgesvd_(char *jobu, char *jobvt, __CLPK_integer *m, __CLPK_integer *n, 
+	//__CLPK_doublereal *a, __CLPK_integer *lda, __CLPK_doublereal *s, __CLPK_doublereal *u, __CLPK_integer *
+	//ldu, __CLPK_doublereal *vt, __CLPK_integer *ldvt, __CLPK_doublereal *work, __CLPK_integer *lwork, 
+	//__CLPK_integer *info);
+	//SUBROUTINE DGESVD( JOBU, JOBVT, M, N, A, LDA, S, U, LDU, VT, LDVT, WORK, LWORK, INFO )
+	dgesvd_(&('A'), &('A'), &(nrows), &(ncols), A, nrows, S, U, nrows, VT, ncols, WORK, LWORK, info);
 	
 	return A;
 }

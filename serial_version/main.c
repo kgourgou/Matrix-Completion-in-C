9679222bad69c6ba5522f79a4660e89c52979219
Kostantinos Gourgoulias <kgourgoulias@gmail.com>
2014-04-19 18:35:09 -0400

    Whatever

--- serial_version/main.c
+++ serial_version/main.c
@@ -8,8 +8,8 @@
 #include "matCompl.h"
 
 int main(void){ 
- int nrows = 4;
- int ncols = 4;
+ int nrows = 2;
+ int ncols = 2;
  int rank = 3;
  double *A = test_mat(rank, nrows, ncols);
  double *B = alloc_array_z(nrows, ncols);
@@ -18,7 +18,7 @@ int main(void){
          
  int i,j;
 
- for(i = 0; i < nrows;i++){
+ /*for(i = 0; i < nrows;i++){
      for(j = 0; j < ncols;j++){
      	if(i == j){
      		B[map(i,j,ncols, 'n')] = 20.0;
@@ -27,11 +27,15 @@ int main(void){
  }
  
  A[1] = 2.0;
+ */
+
+ B[map(0,0,2,'n')] = 1;
+ B[map(0,1,2,'n')] = 0;
  
  print_mat(mm(A, nrows, ncols, A, nrows, ncols),nrows, ncols);
  
 
- print_mat(B, nrows, ncols);
+ //print_mat(B, nrows, ncols);
  //print_mat(mm(B, nrows, ncols, A, nrows, ncols),nrows, ncols);
  
  //print_mat(A, nrows, ncols);

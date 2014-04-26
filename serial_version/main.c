f9174a25513f4601e7d78c17307860e282ee82d2
Kostantinos Gourgoulias <kgourgoulias@gmail.com>
2014-04-26 18:49:10 -0400

    More changes.

--- serial_version/main.c
+++ serial_version/main.c
@@ -7,18 +7,18 @@
 #include "matCompl.h"
 
 int main(void){ 
- int nrows = 2;
- int ncols = 2;
- int rank = 1;
+ int nrows = 4;
+ int ncols = 4;
+ int rank = 2;
          
 
- int numIter = 1000;
+ int numIter = 100;
  int i,j;
  double tau = 1.5*nrows;
- double delta = 1.9;
+ double delta = 1.4;
  
  int omega_c[] = {0}; // places of unknown values.
- int omega[] = {1,2,3}; // places of known values.
+ int omega[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15}; // places of known values.
 
  int ku = 1; // Number of unknown values.
  int kn = (nrows*ncols)-ku; // Number of known values.
@@ -44,7 +44,8 @@ int main(void){
  }
  
  printf("Error = %f\n\n", RMSE2(M, Z, omega_c, ku));
-
+ printf("Known value = %f\n", M[0]);
+ printf("Approximation = %f\n", Z[0]);
 
  free_array(Y);
  free_array(M); 
@@ -54,3 +55,4 @@ int main(void){
  return 0;
 }
  
+

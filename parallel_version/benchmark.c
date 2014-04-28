58927eff1c642a3303e0fc333d386882957cab77
Matthew Brown <mbrown@math.umass.edu>
2014-04-28 09:47:09 -0400

    benchmark function now called in main
    create_omega and all matrix creation handled in main now.  still need
    to implement within the benchmark function:
    *ability to switch between serial and parallel (not vital)
    *ability to take various tolerances (not vital)
    *file output of metrics (VITAL)
    *actual tracking of runtimes

--- parallel_version/benchmark.c
+++ parallel_version/benchmark.c
@@ -4,37 +4,23 @@
 
 /*
 
-  Serial version of matrix completion code. 
+  Parallel version of matrix completion code. 
 	
 */
 
 #include "matCompl.h"
 
 /*
-arguments currently not implemented:
-method
-tol
-file_out
-*/
-
-/*
-outstanding questions:
-should we move "create_omega" out of this?	YES
-should the matrices be created here or be passed in?	YES
+not currently implemented:
+method argument
+tol argument
+file_out argument
+any semblance of tracking time during runs
 */
 
 
-void runBenchmark( char method, int nrows, int ncols, int rank,	int ku, int omega_c[], int numIter, double tol, char* file_out, double tau, double delta){     
-	int i,j;
-	int kn = (nrows*ncols)-ku; // Number of known values.
-	int omega[kn]; // places of known values.
-	create_omega(omega_c, ku, omega, kn, nrows*ncols);
- 
-	double *Y = alloc_array_z(nrows, ncols);
-	double *Z = alloc_array_z(nrows, ncols);
-	double *M = test_mat(rank, nrows, ncols);
-	double *dummyMatrix = alloc_array_z(nrows, ncols);
-	double *dummy2 = alloc_array_z(nrows,ncols);
+void runBenchmark( char method, int nrows, int ncols, int rank,	int ku, int omega_c[], int kn, int omega[], int numIter, double tol, char* file_out, double tau, double delta, double* Y, double* Z, double* M, double* dummyMatrix, double* dummy2){     
+	int i, j;
 
 	for( i = 0; i < numIter; i++){
 		for(j=0; j <  ncols*nrows;j++){
@@ -44,9 +30,5 @@ void runBenchmark( char method, int nrows, int ncols, int rank,	int ku, int omeg
 		Proj_sub(M, Z, omega, kn, dummyMatrix);
 		ma(Y, ncols, nrows, 1.0, dummyMatrix, ncols, nrows, delta, omega, kn, Y);
 	}
-	free_array(Y);
-	free_array(M); 
-	free_array(dummy2);
-	free_array(Z);
-	free_array(dummyMatrix);
+	return RMSE2(M, Z, omega_c, ku);
 }
--- parallel_version/main.c
+++ parallel_version/main.c
@@ -6,49 +6,40 @@
 
 #include "matCompl.h"
 #include "benchmark.h"
+
+
 int main(void){ 
- int nrows = 4;
- int ncols = 4;
- int rank = 1;        
+	int nrows = 4;
+	int ncols = 4;
+	int rank = 1;        
+
+	int numIter = 5;
+	double tau = 1.5*nrows;
+	double delta = 1.4;
+	double tol = 1.0;	//		terrible choice of value, but this isn't implemented yet anyways
+	
+	char* file_out = "test.txt";
+
+	int ku = 3; // Number of unknown values.
+	int omega_c[] = {0, 5, 7}; // places of unknown values.
+	int kn = (nrows*ncols)-ku; // Number of known values.
+	int omega[kn]; // places of known values.
+	create_omega(omega_c, ku, omega, kn, nrows*ncols);
+
+	double *Y = alloc_array_z(nrows, ncols);
+	double *Z = alloc_array_z(nrows, ncols);
+	double *dummyMatrix = alloc_array_z(nrows, ncols);
+	double *dummy2 = alloc_array_z(nrows,ncols);
+	
+	double *M = test_mat(rank, nrows, ncols);
 
 
- int numIter = 1000;
- int i,j;
- double tau = 1.5*nrows;
- double delta = 1.4;
- 
- int ku = 3; // Number of unknown values.
-int omega_c[] = {0, 5, 7}; // places of unknown values.
- int kn = (nrows*ncols)-ku; // Number of known values.
-  int omega[kn]; // places of known values.
- 
- create_omega(omega_c, ku, omega, kn, nrows*ncols);
- 
- double *Y = alloc_array_z(nrows, ncols);
-  double *Z = alloc_array_z(nrows, ncols);
-  double *M = test_mat(rank, nrows, ncols);
- double *dummyMatrix = alloc_array_z(nrows, ncols);
- double *dummy2 = alloc_array_z(nrows,ncols);
+	runBenchmark( 'S', nrows, ncols, rank, ku, omega_c, kn, omega, numIter, tol, file_out, tau, delta, Y, Z, M, dummyMatrix, dummy2);
 
- for( i = 0; i < numIter; i++){
- 
-    for(j=0; j <  ncols*nrows;j++){
-        dummy2[j] = Y[j];
-    }
-    Z = shrink(dummy2, tau, nrows, ncols);
- 	Proj_sub(M, Z, omega, kn, dummyMatrix);
- 	ma(Y, ncols, nrows, 1.0, dummyMatrix, ncols, nrows, delta, omega, kn, Y);
- }
- printf("Error = %f\n\n", RMSE2(M, Z, omega_c, ku));
-
- printf("Known value = %f\n", M[0]);
- printf("Approximation = %f\n", Z[0]);
-
-
- free_array(Y);
- free_array(M); 
- free_array(dummy2);
- free_array(Z);
- free_array(dummyMatrix);
- return 0;
+	free_array(Y);
+	free_array(M); 
+	free_array(dummy2);
+	free_array(Z);
+	free_array(dummyMatrix);
+	return 0;
 }
